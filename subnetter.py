a3b8f5fe2fd3532f030734777c29a221865ffa36
james <jimmy.za@protonmail.com>
2023-02-19 10:23:24 -0600

    calculates sunbnet mask

---
+++ subnetter.py
@@ -0,0 +1,78 @@
+import re
+
+def verifyIp(ip):
+    """ verificacion de la ip """
+    if re.findall("\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}", ip):  # verifica el formato correcto de la ip
+        if not len([True for x in ip.split(".") if int(x) <= 255 and int(x) >= 0]) == 4:  # comprueba que los octetos esten entre 0 y 255
+            print(f"\033[31;1m{ip}\033[0m es una dirección no válida, los octetos deben estar entre 0 y 255, con formato 10.10.4.5")
+            return False
+
+    else:
+        print(f"\033[31;1m{ip}\033[0m es una dirección no válida, los octetos deben estar entre 0 y 255, con formato 10.10.4.5")
+        return False
+
+    return True
+
+def necessaryBits(required):
+    bits = 0
+    for i in range(32):
+        x = 2 ** i - 2
+        if x >= required:
+            bits = i
+            break
+
+    return bits
+
+def getMask(mask, bits, mode="net"):
+    octets = mask.split(".")
+    n_mask = []
+
+    for o in octets:
+        if int(o) == 0:
+            n_oct = "1" * bits + "0" * (8 - bits)
+            n_mask.append(str(int(n_oct, 2)))
+            break
+
+        else:
+            n_mask.append(o)
+
+    while(len(n_mask) < 4):
+        n_mask.append(str(int("0" * 8, 2)))
+
+    return ".".join(n_mask)
+
+def main():
+    """
+    ip_base = input("IP base (ej. 200.35.2.0): ")
+    mask = input("Mascara (ej. 255.255.255.254): ")
+    subnets = input("Cuántas subredes necesitas? (n, para no calcular): ")
+    hosts = input("Cuántos hosts necesitas? (n, para no calcular): ")
+    """
+    ip_base = "132.18.0.0"
+    mask = "255.255.0.0"
+    subnets = "50"
+    hosts = "1000"
+
+    s_bits = h_bits = None
+
+    if verifyIp(ip_base) and verifyIp(mask):
+        if subnets != "n":
+            try:
+                s_bits = necessaryBits(int(subnets))
+                print(f"subnet bits: {s_bits}")
+                n_mask = getMask(mask, s_bits) 
+                print(f"new mask: {n_mask}")
+            except:
+                print(f"\033[31;1m'{subnets}' no válido\033[0m, ingresa un números enteros en subnets y hosts")
+
+        if hosts != "n":
+            try:
+                h_bits = necessaryBits(int(hosts))
+                print(f"host bits: {h_bits}")
+            except:
+                print(f"\033[31;1m'{hosts}' no válido\033[0m, ingresa un números enteros en subnets y hosts")
+    
+    
+
+if __name__ == "__main__":
+    main()
